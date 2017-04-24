122f063e1bc99fcd0d23e0f7b66a9d6fae121fd8
Ilnur Khalilov <ilnur.khalilov@gmail.com>
2017-04-24 14:22:24 +0300

    Applying entity to a manually inputed operator

--- src/Term.jsx
+++ src/Term.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import './Term.css';
 
 
-const Term = ({ className, children, contentState, entityKey }) => {
+const Term = ({ children, contentState, entityKey }) => {
   const data = contentState.getEntity(entityKey).getData();
 
   return (
     <span>
-      <span className={`${className} Term-name`}>
+      <span className="Term-name">
         {children}
       </span>
 
